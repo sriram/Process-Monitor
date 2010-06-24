0bd9e87e6456cc8fa8b38c2dd8129f56f6fb8d1e
Sriram <sriram.varahan@gmail.com>
2010-06-24 16:25:23 +0530

    Added class methods for getting process information.

--- Rakefile
+++ Rakefile
@@ -1,7 +1,10 @@
+require 'rubygems'
+require 'rake'
+
 begin
   require 'jeweler'
   Jeweler::Tasks.new do |gemspec|
-    gemspec.name        = "process-monitor"
+    gemspec.name        = "process_monitor"
     gemspec.summary     = "A gem to monitor processes"
     gemspec.description = "This gem gets the status(running/sleeping/defunct) based on a pattern you provide to it. For example if you have a service which is running and its a ruby service you would need to pass the type(ruby) and a pattern which would identify the process."
     gemspec.email       = "sriram.varahan@gmail.com"
@@ -9,5 +12,5 @@ begin
   end
   Jeweler::GemcutterTasks.new
 rescue LoadError
-  puts "Jeweler not available. Install it with: gem install jeweler"
+  p "Jeweler not available. Install it with: gem install jeweler"
 end
--- lib/process_monitor.rb
+++ lib/process_monitor.rb
@@ -1,38 +1,52 @@
 class ProcessMonitor
   
-  def get_pid(process_type,process_pattern)
+  def self.get_pid(process_type,process_pattern="")
     # Parse the output to fetch the process id.
     process_id_reg_exp = %r{^(.*)\s*}
 
     process_information_lines = `pgrep #{process_type}|xargs ps`.split("\n")
-    pid = nil
+    pids = []
 
-    process_information_lines.each do |process_line|
+    process_information_lines.each_with_index do |process_line,index|
+      next if index == 0
       # The name of the process will be verity-spider
       # This is to make sure we don't kill any other ruby process other than spider.
       if process_line =~ /#{process_pattern}/
         process_id_reg_exp.match(process_line)
-        pid = $1.gsub(/\s*/, "").to_i
+        pid =  $1.gsub(/\s*/, "").to_i
+        pids << pid unless $$ == pid
       end
     end
-    pid
+    if pids.length > 1
+      pids
+    else
+      pids.first
+    end
   end
 
 
-  def self.get_process_status(process_type,process_pattern)
-    pid = get_pid(process_type,process_pattern)
-    `cat /proc/#{pid}/status|grep State`
+  def self.get_process_status(pid=nil)
+    unless pid.nil?
+      `cat /proc/#{pid}/status|grep State`
+    else
+      p "No pid given!"
+    end
   end
 
 
-  def self.process_is_up?(process_type,process_pattern)
-    pid = get_pid(process_type,process_pattern)
-    # The folder /proc/<pid>/ contains information about the process
-    # The file /proc/<pid>/status gives information about the process as to if it is sleeping and so on.
-    # If the folder exists it would mean the process exists.
-    (pid.nil? || !File.exists?("/proc/#{pid}"))? false : true
+  def self.process_is_up?(pid)
+    unless pid.nil?
+      # The folder /proc/<pid>/ contains information about the process
+      # The file /proc/<pid>/status gives information about the process as to if it is sleeping and so on.
+      # If the folder exists it would mean the process exists.
+      (pid.nil? || !File.exists?("/proc/#{pid}"))? false : true
+    else
+       p "No pid given!"
+    end
   end
 
 end
+
+
 
 
